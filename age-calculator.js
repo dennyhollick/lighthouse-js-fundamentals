6c428de209abe330978bc01ef417923e61e818e6
Denny <dennyhollick@gmail.com>
2017-06-09 21:23:28 +0000

    Added age calculator and how many hundreds exercises

---
+++ age-calculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOFBirth, currentYear) {
+  return name + " is " + (currentYear - yearOFBirth) + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
