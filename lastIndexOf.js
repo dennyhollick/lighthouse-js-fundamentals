ebd8b0db73e2216b5d9328aab23258dcddf33635
Denny <dennyhollick@gmail.com>
2017-06-12 14:56:02 +0000

    added lastIndexOf

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+function lastIndexOf(array, value) {
+
+  var i = array.length;
+
+  while (i >= 0 && (array[i] != value)) {
+    i--;
+  }
+  return i;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
